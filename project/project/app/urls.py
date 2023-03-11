d075a552ad489afe841c833070005e417667e349
Skarabajo18 <felipesegovia14@gmail.com>
2023-03-11 04:32:28 -0300

    version 0.0.7

--- project/project/app/urls.py
+++ project/project/app/urls.py
@@ -7,11 +7,12 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
-	path('', views.home, name = 'home'),
-	#path('feed/', views.feed, name = 'feed'),
-	#path('profile/<str:username>/', views.profile, name='profile'),
-	#path('product/<int:id_product>/', views.view_product_post, name='view_product_post'),
-	path('signup/', views.signup, name='signup'),
-	path('login/', LoginView.as_view(template_name = 'login.html'), name = 'login'),
-	#path('logout/', LogoutView.as_view(template_name = 'logout.html'), name = 'logout'),
+    path('', views.home, name='home'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    # path('feed/', views.feed, name = 'feed'),
+    # path('profile/<str:username>/', views.profile, name='profile'),
+    # path('product/<int:id_product>/', views.view_product_post, name='view_product_post'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    # path('logout/', LogoutView.as_view(template_name = 'logout.html'), name = 'logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ social-main/project/project/app/forms.py
@@ -0,0 +1,118 @@
+from .models import (
+    UserAccount,
+    UserProfile,
+    UserRelationship,
+    USER_TYPE
+)
+
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+import datetime
+
+
+###################################################################
+#   Formulario de registro de usuario                             #
+###################################################################
+class SignUpForm(UserCreationForm):
+    
+    first_name = forms.CharField(
+        label = 'Nombres',
+        widget = forms.TextInput(
+            attrs = {
+                'class': 'form-control text-uppercase',
+                'id' : 'input-first-name',
+                'placeholder' : 'Ingresa tus nombres'
+            }
+        )
+    )
+
+    last_name = forms.CharField(
+        label = 'Apellidos',
+        widget = forms.TextInput(
+            attrs = {
+                'class': 'form-control  text-uppercase',
+                'id' : 'input-last-name',
+                'placeholder' : 'Ingresa tus apellidos'
+            }
+        )
+    )
+
+    email = forms.EmailField(
+        label = 'E-mail',
+        widget = forms.EmailInput(
+            attrs = {
+                'class': 'form-control',
+                'id' : 'input-email',
+                'placeholder' : 'Ingresa tu e-mail'
+            }
+        )
+    )
+
+    username = forms.CharField(
+        label = 'Nombre de Usuario',
+        widget = forms.TextInput(
+            attrs = {
+                'class': 'form-control',
+                'id' : 'input-username',
+                'placeholder' : 'Ingresa el nombre de usuario de la cuenta a crear'
+            }
+        )
+    )
+
+
+    password1 = forms.CharField(
+        label = 'Contraseña',
+        widget = forms.PasswordInput(
+            attrs = {
+                'class': 'form-control',
+                'id' : 'input-password1',
+                'placeholder' : 'Ingresa la contraseña de la cuenta a crear'
+            }
+        )
+    )
+
+
+    password2 = forms.CharField(
+        label = 'Confirma la contraseña',
+        widget = forms.PasswordInput(
+            attrs = {
+                'class': 'form-control',
+                'id' : 'input-password2',
+                'placeholder' : 'Confirma la contraseña'
+            }
+        )
+    )
+
+
+    type = forms.ChoiceField(
+    	choices=USER_TYPE,
+        required = True,
+    	label = 'Selecciona el tipo de usuario a crear',
+    	widget = forms.Select(
+            attrs = {
+                'class': 'form-control',
+                'id' : 'select-type-user'
+            }
+        )
+    )
+
+    class Meta:
+
+        model = UserAccount
+        fields = UserCreationForm.Meta.fields + (
+            'type', 'email', 'username', 'password1', 'password2',
+            'first_name', 'last_name',
+        )
+    
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+    
+    def clean_first_name(self):
+        return self.cleaned_data['first_name'].upper()
+    
+    def clean_last_name(self):
+        return self.cleaned_data['last_name'].upper()
+
+
