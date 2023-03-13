86dc1cf26168c38c195ba4f66601e4d2eb34e974
Skarabajo18 <felipesegovia14@gmail.com>
2023-03-13 03:56:21 -0300

    version 0.1.0

---
+++ project/app/static/js/messages/messageLogin.js
@@ -0,0 +1,10 @@
+import Swal from  '../sweetalert2/dist/sweetalert2.all'
+
+
+function showAlert() {
+
+  alert("adasdsa");
+  Swal.fire('Good job!',
+                   'You clicked the button!',
+                   'success');
+}
---
+++ project/app/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from . import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView, LogoutView
+
+
+urlpatterns = [
+	path('', views.home, name = 'home'),
+	#path('feed/', views.feed, name = 'feed'),
+	path('profile/', views.profile_user, name='profile'),
+	#path('product/<int:id_product>/', views.view_product_post, name='view_product_post'),
+	path('signup/', views.signup, name='signup'),
+	path('login/', LoginView.as_view(template_name = 'login.html'), name = 'login'),
+	path('logout/', LogoutView.as_view(), name = 'logout'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
